1b5a487b6720fc9ef2fdcc0dfff5842c20bdae71
Aleksandrs <alex.timofeyev@bk.ru>
2022-02-25 12:09:56 +0300

    Добавляет стрелочные функции, правит значение которое возвращает функция

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 //Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-function getRandomNumbIncluded(min, max) {
+const getRandomNumbIncluded = (min, max) => {
   if (max <= min) {
-    return ('Предел интервала не может быть меньше или равен его началу');
+    return false;
   }
   return Math.floor(Math.random() * (max - min + 1) + min); //Максимум и минимум включаются
-}
+};
 
 getRandomNumbIncluded(1, 5);
 
-function checkStringLength(commentInputString, commentInputStringMax = 140) {
+const checkStringLength = (commentInputString, commentInputStringMax = 140) => {
   if (commentInputString.length > commentInputStringMax) {
-    return ('Длина комментария ограничена 140 символами.');
+    return false;
   }
-  return ('Введите комментарий');
-}
+  return true;
+};
 
 checkStringLength('loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremLoremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremLorem',);
